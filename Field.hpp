ffb5672d5c6f64d1d896b5ef0b17715da8f4a218
Steleac Raul <raul.steleac@student.upt.ro>
2018-04-16 11:23:20 +0300

    Add files via upload

---
+++ Field.hpp
@@ -0,0 +1,27 @@
+@@ -0,0 +1,26 @@
+#ifndef _FIELD_HPP_
+
+#include <iostream>
+#include <vector>
+using std::vector;
+using std::cout;
+using std::endl;
+class Field{
+
+vector<vector <int> > matr_;
+int domain_;
+int w_locw;
+int w_loch;
+
+public:
+  Field();
+  int size();
+  vector<int> &operator [](int i);
+  void move(int nr)
+  bool check_solution();
+
+};
+
+
+
+#endif
